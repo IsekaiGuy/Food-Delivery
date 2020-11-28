1885cda3e1d0e5188770508765420ff68f7ff972
IsekaiGuy <isekaiguy@protonmail.com>
2020-11-28 07:44:52 +0700

    Card generation class has been added.

--- js/main.js
+++ js/main.js
@@ -99,9 +99,10 @@ document.addEventListener("keydown", e => {
 const deadline = '2020-12-05';
 
 function getTimeRemaining(endtime) {
-        const t = Date.parse(endtime) - Date.parse(new Date()),
-        days = Math.floor(t/ (1000 * 60 * 60 * 24)),
-        hours = Math.floor((t/ (1000 * 60 * 60) % 24)),
+    
+  const t = Date.parse(endtime) - Date.parse(new Date()),
+        days = Math.floor(t / (1000 * 60 * 60 * 24)),
+        hours = Math.floor((t / (1000 * 60 * 60) % 24)),
         minutes = Math.floor((t / 1000 / 60) % 60),
         seconds = Math.floor((t / 1000) % 60);
 
@@ -149,5 +150,70 @@ const timer = document.querySelector(selector),
 
 setClock('.timer', deadline);
 
-});
+//Используем классы для карточек
 
+class Menu {
+    constructor(img, alt, subtitle, descr, price, parentSelector, ...classes) {
+            this.img = img;
+            this.alt = alt;
+            this.subtitle = subtitle;
+            this.descr = descr;
+            this.price = price;
+            this.classes = classes;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.changeToUAH();
+    }
+
+    changeToUAH() {
+        this.price = this.price * this.transfer;
+    }
+
+    render() {
+        const div = document.createElement('div');
+        if(this.classes.length === 0) {
+            this.div = 'menu__item';
+            div.classList.add(this.div);
+        } else {
+            this.classes.forEach(className => div.classList.add(className));
+        }
+
+        div.innerHTML = `
+                <img src="${this.img}" alt="${this.alt}">
+                <h3 class="menu__item-subtitle">${this.subtitle}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>`;
+        this.parent.append(div);
+    }
+}
+
+new Menu(
+    'img/tabs/vegy.jpg', 
+    'vegy', 
+    'Меню "Фитнес"', 
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+     9, 
+     '.menu .container', 
+     'menu__item').render();
+new Menu(
+    'img/tabs/elite.jpg',
+    'elite', 
+    'Меню “Премиум”', 
+    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!', 
+    14, 
+    '.menu .container', 
+    'menu__item').render();
+new Menu(
+    'img/tabs/post.jpg', 
+    'post', 
+    'Меню "Постное"', 
+    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.', 
+    21, 
+    '.menu .container', 
+    'menu__item').render();
+
+});
